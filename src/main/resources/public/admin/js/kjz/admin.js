e4f70510ed0c431af1f3bcbe84bd1540ad6530af
hkh <296739395@qq.com>
2018-03-22 14:07:23 +0800

    增加文章置顶，广告，敏感词替换功能

--- src/main/resources/public/admin/js/kjz/admin.js
+++ src/main/resources/public/admin/js/kjz/admin.js
@@ -1,5 +1,5 @@
-var listURL = "127.0.0.1:9090/admin/getAdminAdminList";
-var editURL = "127.0.0.1:9090/admin/editAdmin";
+var listURL = "/admin/getAdminAdminList";
+var editURL = "/admin/editAdmin";
 $('#tableList').bootstrapTable({
 	url : listURL, // 请求后台的URL（*）
 	method : 'POST', // 请求方式（*）
